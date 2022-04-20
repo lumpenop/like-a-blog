ec4b1bca8a88972c88497f10c2bdc0104df09cf7
lumpenop <swk5373@gmail.com>
2022-04-21 01:38:22 +0900

    Feat: https, cors, key 추가 server - key, cert 추가/ https 적용/cors 설정 client - package.json에 key 설정 추가

--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const cors = require('cors');
+
 const dotenv = require('dotenv');
-const app = express();
+
 dotenv.config();
-const port = process.env.SERVER_PORT;
+const app = express();
+
+const PORT = process.env.SERVER_PORT || 4001;
 
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
-app.use('/', '');
 
-app.listen(port, ()=>{
-    console.log(`server is starting on ${port}`);
-})
+const corsOptions = {
+    origin: `https://localhost:3000`,
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS'],
+  }
+  app.use(cors(corsOptions));
+
+
+
+app.use('/board', '');
+
+
+
+let server;
+
+// 인증서 파일들이 존재하는 경우에만 https 프로토콜을 사용하는 서버를 실행합니다.
+// 만약 인증서 파일이 존재하지 않는경우, http 프로토콜을 사용하는 서버를 실행합니다.
+// 파일 존재여부를 확인하는 폴더는 서버 폴더의 package.json이 위치한 곳입니다.
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+  server = https
+    .createServer(
+      {
+        key: fs.readFileSync(__dirname + `/` + 'key.pem', 'utf-8'),
+        cert: fs.readFileSync(__dirname + `/` + 'cert.pem', 'utf-8'),
+      },
+      app
+    )
+    .listen(PORT);
+} else {
+  server = app.listen(PORT)
+}
